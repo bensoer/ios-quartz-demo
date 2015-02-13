f7b83990573ea91cf9e1a2002a63ea7718cfce31
bensoer <ikben@shaw.ca>
2015-02-12 22:26:56 -0800

    Completed Quartz simple demo. Need to add drawing saving

--- quartz2D/ViewController.swift
+++ quartz2D/ViewController.swift
@@ -22,6 +22,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeColor(sender: UISegmentedControl) {
+        let drawingColorSelection = DrawingColor(rawValue: UInt(sender.selectedSegmentIndex));
+        if let drawingColor = drawingColorSelection{
+            let quartz2DView = view as quartz2D.quartz2DView; //textbook makes a mistake here
+            switch drawingColor {
+            case .Red:
+                quartz2DView.currentColor = UIColor.redColor();
+                quartz2DView.useRandomColor = false;
+            case .Blue:
+                quartz2DView.currentColor = UIColor.blueColor();
+                quartz2DView.useRandomColor = false;
+            case .Yellow:
+                quartz2DView.currentColor = UIColor.yellowColor();
+                quartz2DView.useRandomColor = false;
+            case .Green:
+                quartz2DView.currentColor = UIColor.greenColor();
+                quartz2DView.useRandomColor = false;
+            case .Random:
+                quartz2DView.useRandomColor = true;
+            default:
+                break;
+            }
+            
+        }
+    }
 
+    @IBAction func changeShape(sender: AnyObject) {
+        let shapeSelection = Shape(rawValue: UInt(sender.selectedSegmentIndex));
+        if let shape = shapeSelection {
+            let quartz2DView = view as quartz2D.quartz2DView;
+            quartz2DView.shape = shape;
+            colorControl.hidden = shape == Shape.Image;
+        }
+    }
 }
 
